033e47406a95c363ae5f2989d7a16261e50bb2b8
Dzhurunov Tamerlan Serzhanovich <dzhurunovt@gmail.com>
2023-11-16 15:03:43 +0600

    feat: add weather module

--- main.py
+++ main.py
@@ -3,14 +3,37 @@ from datetime import datetime, timedelta
 from telegram import Update
 from telegram.ext import CommandHandler, ApplicationBuilder, ContextTypes
 import requests
+from pyowm import OWM
 
-TELEGRAM_TOKEN = '6898158317:AAGW_WCZ5LoaAyxuxl5r2b0pMIfFdJ-XUOs'
-WEBEX_ACCESS_TOKEN = 'OWNkMWVlMzctOTIzYy00NTQ0LTkyNTMtMjFjNWYwYmJiMTg4MWNmYmU2YzItNjNh_PE93_64bb227d-594d-4030-a56f-373e324be165'
+TELEGRAM_TOKEN = '6611113927:AAHxW5DPtPnv2LiBjsdxtmh_gZ0Be7bsXsc'
+WEBEX_ACCESS_TOKEN = 'NmZlZGJkMDktZGE0Ny00OWQxLWI4YmEtMGQxMjZiMmM2ZDE2NmQwMzMxMTktMTk3_PE93_64bb227d-594d-4030-a56f-373e324be165'
 WEBEX_SITE_URL = 'webexapis.com'
+OPENWEATHERMAP_API_KEY = '3fdb2d7a03757ca1b310cd4a245c9355'
+
+
+async def send_link_to_mentioned_users(update: Update, context: ContextTypes.DEFAULT_TYPE,
+                                       meeting_link: str) -> None:
+    chat_id = update.message.chat_id
+
+    mentioned_users = [
+        user.username for user in update.message.entities
+        if user.type == 'mention' and user.user.username
+    ]
+
+
+def get_weather():
+    owm = OWM(OPENWEATHERMAP_API_KEY)
+    mgr = owm.weather_manager()
+    observation = mgr.weather_at_place("Almaty,Kazakhstan")
+    w = observation.weather
+    temperature = w.temperature('celsius')['temp']
+    weather_status = w.status
+    return f'Текущая температура: {temperature}°C\nСостояние погоды: {weather_status}'
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('Привет! Отправь мне команду /newmeeting, чтобы создать новую видеоконференцию Webex.')
+    await update.message.reply_text(
+        'Привет! Отправь мне команду /newmeeting, чтобы создать новую видеоконференцию Webex.')
 
 
 async def new_meeting(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -28,7 +51,13 @@ async def new_meeting(update: Update, context: ContextTypes.DEFAULT_TYPE):
     response = requests.post(f'https://{WEBEX_SITE_URL}/v1/meetings', headers=headers, json=data)
     if response.status_code == 200:
         meeting_link = response.json()['webLink']
-        await update.message.reply_text(f'Ссылка на вашу видеоконференцию: {meeting_link}')
+
+        # Получаем информацию о погоде
+        weather_info = get_weather()
+
+        # Отправляем сообщение с ссылкой на видеоконференцию и информацией о погоде
+        await update.message.reply_text(
+            f'Ссылка на вашу видеоконференцию: {meeting_link}\n\n{weather_info}')
     else:
         await update.message.reply_text('Произошла ошибка при создании видеоконференции.')
 
@@ -42,5 +71,5 @@ def main():
     app.run_polling()
 
 
-if __name__ == '__main__':
+if name == 'main':
     main()
